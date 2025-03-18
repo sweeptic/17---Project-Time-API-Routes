90c6634b2c32038b79817b558d44f376b35caafd
Attila Horvath <sweeptic@gmail.com>
2025-03-18 15:11:25 +0100

    007 Running MongoDB Queries From Inside API Routes

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,6 @@
-export default function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+export default async function handler(req, res) {
   if (req.method === 'POST') {
     const userEmail = req.body.email;
 
@@ -6,6 +8,15 @@ export default function handler(req, res) {
       res.status(422).json({ message: 'Invalid email address.' });
       return;
     }
+
+    const client = await MongoClient.connect(
+      'mongodb+srv://mongouser123:Hardfloor888888@cluster0.5mx6g.mongodb.net/newsletter?retryWrites=true&w=majority&appName=Cluster0'
+    );
+
+    const db = client.db();
+    await db.collection('emails').insertOne({ email: userEmail });
+
+    client.close();
 
     res.status(201).json({ message: 'Signed up' });
   }
